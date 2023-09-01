d74b54151b06b85bcc0ae6565beda795e31ea4de
xtrilogis <julia.winkler15@freenet.de>
2023-09-01 15:00:19 +0200

    FIX: statistics

--- src/stats/statistics.py
+++ src/stats/statistics.py
@@ -1,10 +1,10 @@
 """Calculate some basic statistics"""
 from pathlib import Path
 from typing import List, Any, Tuple
-
 from moviepy.editor import VideoFileClip
 import os
 import time
+
 from assets.constants import video_extensions
 
 
@@ -12,47 +12,31 @@ def get_raw_material_duration(path: Path) -> Tuple[float, List[str], List[List[A
     total_duration = 0.0
     problems: List[str] = []
     duration_per_day: List[List[Any]] = []
-    try:
-        folders = os.listdir(path)
-        for folder in folders:
-            if os.path.isdir(folder):
-                current_day_duration = 0
-                for root, _, files in os.walk(os.path.join(path, folder)):
-                    for file in files:
-                        _, ext = os.path.splitext(file)
-                        if ext in video_extensions:
-                            duration = _get_duration(root, file)
-                            total_duration += duration
-                            current_day_duration += duration
-                duration_per_day.append(
+
+    for folder in path.iterdir():
+        if folder.is_dir():
+            current_day_duration = 0
+            for element in folder.glob('**/*'):
+                if element.suffix in video_extensions:
+                    try:
+                        duration = _get_duration(element)
+                        total_duration += duration
+                        current_day_duration += duration
+                    except AttributeError:
+                        problems.append(element.name)
+            duration_per_day.append(
                     [
                         folder,
                         time.strftime("%H:%M:%S", time.gmtime(current_day_duration)),
                     ]
                 )
-        return total_duration, problems, duration_per_day
-    except Exception as e:
-        return -1.0, [], []
+    return total_duration, problems, duration_per_day
 
 
-def _get_duration(root, file):
-    file_fullpath = os.path.join(root, file)
-    try:
-        duration = VideoFileClip(file_fullpath).duration
-        print(f"File: {file}, Dauer: {duration}s")
-        return duration
-    except Exception as e:
-        problems.append(file_fullpath)
-        return 0
-
-
-def _count_all(path: Path) -> int:
-    """Count all files in the given directory"""
-    total = 0
-    for root, _, files in os.walk(path):
-        for file in files:
-            total += 1
-    return total
+def _get_duration(file_fullpath: Path):
+    duration = VideoFileClip(str(file_fullpath)).duration
+    print(f"File: {file_fullpath.name}, Dauer: {duration}s")
+    return duration
 
 
 def percent_selected(raw: Path, selected: Path) -> str:
@@ -61,12 +45,9 @@ def percent_selected(raw: Path, selected: Path) -> str:
     return f"{sel_num} von {raw_num} benutzt. ({sel_num/raw_num*100}%)"
 
 
-if __name__ == "__main__":
-    roots = "D:\\Users\\Wisdom\\Lernen\\Coding_Python\\Zlf_sort\\Dateien\\Test dateien" \
-            "\\2022\\Zeltlager 2022\\Zeltlagerfilm 2022\\Rohmaterial\\"
-    total_duration, problems, duration_per_day = get_raw_material_duration(Path(roots))
-    print(
-        f'Summe: {total_duration}s | {time.strftime("%H:%M:%S", time.gmtime(total_duration))}\n'
-        f"Probleme: {problems}\n"
-        f"Tage: {duration_per_day}"
-    )
+def _count_all(path: Path) -> int:
+    """Count all files in the given directory"""
+    total = 0
+    for root, _, files in os.walk(path):
+        total += len(files)
+    return total
