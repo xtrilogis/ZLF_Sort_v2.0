0559082dc9269190a2c4f75eb795eaaddb57e8ff
xtrilogis <julia.winkler15@freenet.de>
2023-09-03 15:26:01 +0200

    ADD: empty pytest files

---
+++ tests/test_util_methods.py
@@ -0,0 +1,22 @@
+# mock_copy_file assert calls with right argument
+# dummy_df, good and bad
+# test with mock_copy_file mit raise FileNotFound
+def test_prepare_df():
+    pass
+
+
+def test_copy_section():
+    pass
+
+
+def test_copy_selections():
+    pass
+
+
+def test_search_columns():
+    pass
+
+
+def test_copy_pictures_with_rating():
+    pass
+
