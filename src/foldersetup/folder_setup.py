c7ab8225ed709c3eb2e6f3ed01fdd7a600e52d4e
xtrilogis <julia.winkler15@freenet.de>
2023-09-15 12:05:40 +0200

    feat: test foldersetup

--- src/foldersetup/folder_setup.py
+++ src/foldersetup/folder_setup.py
@@ -2,6 +2,7 @@ import locale
 from datetime import datetime, timedelta
 from pathlib import Path
 from typing import List
+from fileopertations.filemethods import create_folder
 
 locale.setlocale(locale.LC_TIME, 'de_DE.utf8')
 
@@ -12,26 +13,26 @@ def create_folder_structure(parent: Path, date: datetime):
 
     ext_m_subfolder = ["Dokumente", "Musik", "Grafiken"]  # Subfolder for 'ext. Material'
 
-    parent = create_folder(parent, f'Zeltlagerfilm {date.year}')
+    parent = create_folder(parent=parent, folder=f'Zeltlagerfilm {date.year}')
 
     for folder in root_subfolder:
         create_folder(parent=parent, folder=folder)
 
-    raw_material_folder(parent=parent, date=date)
+    _create_raw_material_folder(parent=parent, date=date)
 
     sub = parent / "ext. Material"
     for folder in ext_m_subfolder:
         create_folder(parent=sub, folder=folder)
 
 
-def raw_material_folder(parent: Path, date: datetime):
+def _create_raw_material_folder(parent: Path, date: datetime):
     sub = parent / "Rohmaterial"
-    raw_subfolder = day_folder(date=date)  # Subfolder for 'Rohmaterial'
+    raw_subfolder = _day_folder(date=date)  # Subfolder for 'Rohmaterial'
     for folder in raw_subfolder:
         create_folder(parent=sub, folder=folder)
 
 
-def day_folder(date: datetime) -> List[str]:
+def _day_folder(date: datetime) -> List[str]:
     """Creates a folder for every day with subfolders 'Bilder' and 'Videos'"""
     letter = 97  # char for 'a'
     day_folders = []
@@ -42,10 +43,3 @@ def day_folder(date: datetime) -> List[str]:
     day_folders.append(f'{chr(letter + 11)}-Sonstiges/Bilder')
     day_folders.append(f'{chr(letter + 11)}-Sonstiges/Videos')
     return day_folders
-
-
-def create_folder(parent: Path, folder: str) -> Path:
-    create: Path = parent.joinpath(folder)
-    if not create.exists():
-        create.mkdir(parents=True)
-    return create
--- tests/test_folder_structure.py
+++ tests/test_folder_structure.py
@@ -1,21 +1,17 @@
-import os.path
 from datetime import datetime
 from pathlib import Path
 from unittest import mock
 
-from PyQt5.QtCore import QDate
-
-import src.foldersetup.folder_structure
-from foldersetup import folder_structure
-
-ROOT = Path.cwd() / "testData"
+from foldersetup.folder_setup import create_folder_structure
 
 
-@mock.patch("foldersetup.folder_structure.create_folder")
-def test_folder_creation(mock_create_folder):
-    mock_create_folder.return_value = Path(".")
+@mock.patch("foldersetup.folder_setup.create_folder")
+def test_folder_creation(mock_create_folder, test_data_path):
     date = datetime.now()
+    mock_create_folder.return_value = test_data_path / f'Zeltlagerfilm {date.year}'
 
-    folder_structure.create_folder_structure(ROOT, date)
+    create_folder_structure(test_data_path, date)
     assert mock_create_folder.called
     assert mock_create_folder.call_count == 31  # wegen 31 Ordnern
+    assert mock_create_folder.call_args_list[0].kwargs['parent'] == test_data_path
+    assert mock_create_folder.call_args_list[0].kwargs['folder'] == f'Zeltlagerfilm {date.year}'
