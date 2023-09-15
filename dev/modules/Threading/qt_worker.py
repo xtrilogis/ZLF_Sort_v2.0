b66e671d6ffdbf06c1d69c6225168b155647c723
xtrilogis <julia.winkler15@freenet.de>
2023-09-15 10:01:10 +0200

    feat: sample for new threading method

--- dev/modules/Threading/qt_worker.py
+++ dev/modules/Threading/qt_worker.py
@@ -3,15 +3,21 @@ from PyQt5.QtCore import *
 
 
 class Worker(QThread):
-    partial_completion = pyqtSignal()
+    def __init__(self, ui):
+        super().__init__()
+        self.ui = ui
+
+    partial_completion = pyqtSignal(str)
     process_completed = pyqtSignal()
 
-    @pyqtSlot()
-    def some_process(self):
+    @pyqtSlot(str)
+    def some_process(self, name):
+        print(name)
         print("Start processing")
+        self.partial_completion.emit("Start")
         self.do_task_one()
         print("first block finished")
-        self.partial_completion.emit()
+        self.partial_completion.emit("first block finished")
         self.do_task_tow()
         print("finished processing")
         self.process_completed.emit()
