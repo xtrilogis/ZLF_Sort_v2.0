d905d96b9c3e7a3237cd4a08980882a655d4f3da
xtrilogis <julia.winkler15@freenet.de>
2024-03-23 15:55:31 +0100

    !

--- src/tests/test_validation.py
+++ src/tests/test_validation.py
@@ -12,60 +12,60 @@ def test_validate_folder(testdata_path):
 
 
 def test_validate_excel_file(testdata_path):
-    errors = validation.validate_excel_file(excel_file=testdata_path / "ok_data.xlsx")
-    assert len(errors) == 0
+    # should not raise an error
+    validation.validate_excel_file(excel_file=testdata_path / "ok_data.xlsx")
+
     files = ["not_existing.xlsx", "missing_column.xlsx", "missing_sheet.xlsx"]
     for file in files:
         excel_path = testdata_path / file
 
-        errors = validation.validate_excel_file(excel_file=excel_path)
-        assert len(errors) != 0
+        with pytest.raises(ValueError):
+            validation.validate_excel_file(excel_file=excel_path)
 
 
 def test_validate_setup(testdata_path):
     good_path = testdata_path
-    errors = validation.validate_setup_path(good_path)
-    assert len(errors) == 0
+    validation.validate_setup_path(good_path)
 
-    not_existing = Path("C:/some/path")
-    errors = validation.validate_setup_path(not_existing)
-    assert len(errors) != 0
+    with pytest.raises(ValueError):
+        validation.validate_setup_path(testdata_path / "non existing")
 
     bad_path = testdata_path / "Rohmaterial"
-    errors = validation.validate_setup_path(bad_path)
-    assert len(errors) != 0
+    with pytest.raises(ValueError):
+        validation.validate_setup_path(bad_path)
 
 
-def test_validate_raw(get_raw_input, get_raw_input_non_valids):
+def test_validate_raw(get_raw_input, get_raw_input_non_valid):
     with pytest.raises(ValueError):
         validation.validate_raw(get_raw_input)
-    for input_ in get_raw_input_non_valids:
+    for input_ in get_raw_input_non_valid:
         with pytest.raises(ValueError):
             validation.validate_raw(input_)
 
 
 def test_validate_util_good(testdata_path):
     files = ["ok_data.xlsx", "ok_empty.xlsx"]
-    raw_path = testdata_path / "Rohmaterial"
+    raw_path = testdata_path / "util/Rohmaterial"
     for file in files:
         excel_path = testdata_path / file
 
-        errors = validation.validate_util_paths(raw_path, excel_path)
-        assert len(errors) == 0
+        validation.validate_util_paths(raw_path, excel_path)
 
 
 def test_validate_util_bad(testdata_path):
     files = ["duplicated_data.xlsx", "missing_column.xlsx", "missing_sheet.xlsx"]
-    raw_path = testdata_path / "Rohmaterial"
+    raw_path_good = testdata_path / "util/Rohmaterial"
     for file in files:
-        excel_path = testdata_path / file
+        excel_path_bad = testdata_path / file
 
-        errors = validation.validate_util_paths(raw_path, excel_path)
-        assert len(errors) != 0
+        with pytest.raises(ValueError) as e:
+            validation.validate_util_paths(raw_path_good, excel_path_bad)
+
+
         if "duplicated" in file:
-            assert "07-27-Mi_001.MP4" in errors
+            assert "07-27-Mi_001.MP4" in e.value.args[0]
 
     good_excel_path = testdata_path / "ok_data.xlsx"
-    raw_path = testdata_path / "NichtExistent"
-    errors = validation.validate_util_paths(raw_path, good_excel_path)
-    assert len(errors) != 0
+    raw_path_bad = testdata_path / "NichtExistent"
+    with pytest.raises(ValueError):
+        validation.validate_util_paths(raw_path_bad, good_excel_path)
