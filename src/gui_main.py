3a3a7d3c8d4ca46bcd71525baaa72b45d0cd7799
xtrilogis <julia.winkler15@freenet.de>
2024-04-01 11:28:40 +0200

    refactor: unnecessary prints

--- src/gui_main.py
+++ src/gui_main.py
@@ -562,11 +562,9 @@ class MainWindow(QMainWindow):
 
 def main(app=QApplication(sys.argv)):
     window = MainWindow()
-    print(7)
     window.show()
     sys.exit(app.exec())
 
 
 if __name__ == "__main__":
     main()
-    print(5)
