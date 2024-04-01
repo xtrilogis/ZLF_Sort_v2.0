f75e95157a7bfe876cca97fde39023afe13368a1
xtrilogis <julia.winkler15@freenet.de>
2024-04-01 19:29:43 +0200

    fix: test full

--- src/tests/integration-tests/test_raw_integration.py
+++ src/tests/integration-tests/test_raw_integration.py
@@ -9,7 +9,7 @@ from assethandling.basemodels import ExcelInput, ExcelOption, RawTabInput
 from gui_main import main
 
 
-@patch("src.runner.runners.raw_methods.copy_file")
+@patch("runner.runners.raw_methods.copy_file")
 @patch("sys.exit")
 @patch("gui_main.MainWindow.get_raw_input")
 def test_run_correct_structure(mock_input, _, mock_fn):
@@ -32,7 +32,7 @@ def test_run_correct_structure(mock_input, _, mock_fn):
 
 
 @patch("PyQt5.QtWidgets.QInputDialog.getItem")
-@patch("src.runner.runners.raw_methods.copy_file")
+@patch("runner.runners.raw_methods.copy_file")
 @patch("sys.exit")
 @patch("gui_main.MainWindow.get_raw_input")
 def test_run_correct_structure_errors(mock_input, _, mock_fn, mock_dialog):
@@ -83,12 +83,12 @@ def test_run_rename_errors(mock_input, _, mock_fn):
     assert mock_fn.call_count == 0
 
 
-@patch("PyQt5.QtWidgets.QInputDialog.getText")
+@patch("PyQt5.QtWidgets.QInputDialog.getItem")
 @patch("excel.excelmethods.save_sheets_to_excel")
 @patch("sys.exit")
-@patch("src.main.gui_main.MainWindow.get_raw_input")
+@patch("gui_main.MainWindow.get_raw_input")
 def test_excel_creation_override(mock_input, _, mock_fn, mock_dialog):
-    mock_dialog.return_value = "j", True
+    mock_dialog.return_value = "Ja", True
     mock_input.return_value = RawTabInput(
         raw_material_folder=TEST_PATH,
         first_folder_date=TEST_DATE,
@@ -96,17 +96,19 @@ def test_excel_creation_override(mock_input, _, mock_fn, mock_dialog):
         picture_folder=TEST_PATH,
     )
     main()
-    assert mock_fn.call_count == 2
-    assert mock_fn.call_args_list[0][1]["sheet_name"] == "Videos"
-    assert mock_fn.call_args_list[1][1]["sheet_name"] == "Bilder"
+    assert mock_fn.call_count == 1
+    assert mock_fn.call_args_list[0][1]['path'] == Path(TEST_PATH) / "Zeltlagerfilm custom.xlsx"
+    assert "Videos" in mock_fn.call_args_list[0][1]['sheets'].keys()
+    assert "Bilder" in mock_fn.call_args_list[0][1]['sheets'].keys()
+    assert excel_input_manual.picture_columns[0] in mock_fn.call_args_list[0][1]['sheets']["Bilder"].columns
 
 
-@patch("PyQt5.QtWidgets.QInputDialog.getText")
+@patch("PyQt5.QtWidgets.QInputDialog.getItem")
 @patch("excel.excelmethods.save_sheets_to_excel")
 @patch("sys.exit")
-@patch("src.main.gui_main.MainWindow.get_raw_input")
+@patch("gui_main.MainWindow.get_raw_input")
 def test_excel_creation_no_override(mock_input, _, mock_fn, mock_input_dialog):
-    mock_input_dialog.return_value = "n", True
+    mock_input_dialog.return_value = "Nein", True
     mock_input.return_value = RawTabInput(
         raw_material_folder=TEST_PATH,
         first_folder_date=datetime.now(),
@@ -117,10 +119,10 @@ def test_excel_creation_no_override(mock_input, _, mock_fn, mock_input_dialog):
     assert mock_fn.call_count == 0
 
 
-@patch("src.main.runner.runners.raw_methods.save_sheets_to_excel")
-@patch("src.main.runner.runners.raw_methods.create_emtpy_excel")
+@patch("runner.runners.raw_methods.save_sheets_to_excel")
+@patch("runner.runners.raw_methods.create_emtpy_excel")
 @patch("sys.exit")
-@patch("src.main.gui_main.MainWindow.get_raw_input")
+@patch("gui_main.MainWindow.get_raw_input")
 def test_fill_excel_creation(mock_input, _, mock_create, mock_save):
     excel_input: ExcelInput = ExcelInput(
         option=ExcelOption.CREATE,
@@ -128,7 +130,7 @@ def test_fill_excel_creation(mock_input, _, mock_create, mock_save):
         folder=TEST_PATH,
     )
     mock_input.return_value = RawTabInput(
-        raw_material_folder=TEST_PATH,
+        raw_material_folder=TEST_PATH / "raw/structured1",
         first_folder_date=TEST_DATE,
         excel=excel_input,
         picture_folder=TEST_PATH,
@@ -255,18 +257,17 @@ def test_run_create_picture_folder_errors(mock_input, _, mock_fn, __):
     assert mock_fn.call_count == 0
 
 
-@patch("src.main.runner.runners.raw_methods.create_emtpy_excel")
-@patch("src.main.runner.runners.raw_methods.save_sheets_to_excel")
+@patch("runner.runners.raw_methods.create_emtpy_excel")
+@patch("runner.runners.raw_methods.save_sheets_to_excel")
 @patch("pathlib.Path.rename")
 @patch("pathlib.Path.mkdir")
-@patch("src.main.runner.runners.raw_methods.copy_file")
+@patch("runner.runners.raw_methods.copy_file")
 @patch("sys.exit")
-@patch("src.main.gui_main.MainWindow.get_raw_input")
+@patch("gui_main.MainWindow.get_raw_input")
 def test_process_raw_full(
     mock_input, _, mock_copy, __, mock_rename, mock_save, mock_create
 ):
     mock_input.return_value = RawTabInput(
-        do_structure=True,
         do_rename=True,
         fill_excel=True,
         create_picture_folder=True,
@@ -280,7 +281,7 @@ def test_process_raw_full(
         picture_folder=TEST_PATH,
     )
     main()
-    assert mock_copy.call_count == 28 + 18
-    assert mock_rename.call_count == 28
+    assert mock_copy.call_count == 19
+    assert mock_rename.call_count == 29
     assert mock_save.call_count == 1
     assert mock_create.call_count == 0
