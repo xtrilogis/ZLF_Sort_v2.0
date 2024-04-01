6f9b67ff1c1a84b94f66069b20a1b37f41d17d7f
xtrilogis <julia.winkler15@freenet.de>
2024-04-01 15:54:01 +0200

    feat: setup_folder integration test

--- src/tests/integration-tests/test_setup_integration.py
+++ src/tests/integration-tests/test_setup_integration.py
@@ -1,3 +1,4 @@
+"""Just click the "Erstellen" button no inputs needed."""
 from datetime import datetime
 from pathlib import Path
 from unittest.mock import patch
@@ -5,35 +6,37 @@ from unittest.mock import patch
 from assethandling.basemodels import FolderTabInput
 from gui_main import main
 
+TEST_PATH = Path.cwd().parent / "testData"
 
 @patch("src.runner.runners.setup_methods.create_folder")
 @patch("sys.exit")
-@patch("src.gui_main.MainWindow.get_folder_input")
-def test_folder_setup(mock_input, _, mock_fn):
+@patch("gui_main.MainWindow.get_folder_input")
+def test_folder_setup(mock_input, _, mock_fn, dummy_folder):
     mock_input.return_value = FolderTabInput(
-        folder=Path.cwd(), date=datetime(2023, 10, 5)
+        folder=dummy_folder, date=datetime(2023, 10, 5)
     )
     mock_fn.return_value = Path("test/path/")
 
     main()
     assert mock_fn.call_count == 31
+    assert mock_fn.call_args_list[1].kwargs["parent"] == Path("test/path")
 
 
 # test not a valid folder contains Rohmaterial, not a folder, not existing
 # test if (either send_problems or) input error
 
-TEST_PATH = Path.cwd() / "testData"
-
-
 @patch("src.runner.runners.setup_methods.create_folder")
 @patch("sys.exit")
-@patch("src.gui_MainWindow.get_folder_input")
-def test_folder_setup_errors(mock_input, sys, mock_fn):
+@patch("gui_main.MainWindow.get_folder_input")
+def test_folder_setup_errors(mock_input, _, mock_fn, testdata_path, dummy_file):
+    """Test if errors are handled appropriately
+    three button pushes with three pop ups"""
     mock_input.side_effect = [
-        FolderTabInput(folder="Path.cwd()", date=datetime(2023, 10, 5)),
+        FolderTabInput(folder="Not/existent/folder/", date=datetime(2023, 10, 5)),
         FolderTabInput(
-            folder=TEST_PATH / "Rohmaterial/structured1", date=datetime(2023, 10, 5)
+            folder=testdata_path / "Rohmaterial/structured1", date=datetime(2023, 10, 5)
         ),
+        FolderTabInput(folder=dummy_file, date=datetime(2023, 10, 5)),
     ]
     mock_fn.return_value = Path("test/path/")
 
